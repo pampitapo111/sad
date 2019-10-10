6de7871403ffcfb084a5a074d606d092a2b42510
pampitapo111 <33424184+pampitapo111@users.noreply.github.com>
2019-10-10 15:08:30 +0800

    Update about.blade.php

--- resources/views/website/about.blade.php
+++ resources/views/website/about.blade.php
@@ -24,15 +24,9 @@
         <div>
             <h2>Core</h2> 
             <p>
-                Passion
-               </p>
-               <p>
-	            Excellence
-                </p>
-                <p>
-	            Safety
-                </p>
-                <p>
+            PASSION, EXCELLENCE, SAFETY.
+            </p>
+            <p>
 		Passion for building, designing, and construction that develops our employees and enrich the lives of our clients. We keep our commitments and have integrity on what we do. 
 		Our commitment to excellence supports our entire operation. We aspire to be the best and take pride in our workmanship.
 		We ensure the safety of our employees and workers. It is our principal responsibility to guarantee and provide safety training for our employees and workers.
